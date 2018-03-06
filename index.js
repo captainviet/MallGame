c6814e5c2a6da92837e4c5fb04562c7d31e9322e
Vince Dang <xuanvu001@e.ntu.edu.sg>
2018-03-07 00:22:30 +0800

    Enable koa-logging

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
+const logger = require('koa-logger')
 
 const config = require('./config')
 const serve = require('koa-send')
 const router = require('./router')
 
 const app = new Koa()
+app.use(logger())
 
 app.use(router.routes()).use(router.allowedMethods())
 
